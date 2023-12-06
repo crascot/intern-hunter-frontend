cd66fb6c28f7c64385ef14eb780b75de1cf8209c
Yaroslav <60184959+crascot@users.noreply.github.com>
2023-12-06 22:13:35 +0600

    --force

--- src/pages/Profile/Description/Description.tsx
+++ src/pages/Profile/Description/Description.tsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import s from './Description.module.css'
 
 const Description = () => {
   return (
-    <div>Description</div>
+    <div className={s.container}>
+      <div className={s.info}>
+        <span>
+          <h4>Job Description</h4>
+          <p>
+            Velstar is a Shopify Plus agency, and we partner with brands to help them grow, we also do the same with our people!
+          </p>
+          <p>
+            Here at Velstar, we don't just make websites, we create exceptional digital experiences that consumers love. Our team of designers, developers, strategists, and creators work together to push brands to the next level. From Platform Migration, User Experience & User Interface Design, to Digital Marketing, we have a proven track record in delivering outstanding eCommerce solutions and driving sales for our clients.
+          </p>
+          <p>
+            The role will involve translating project specifications into clean, test-driven, easily maintainable code. You will work with the Project and Development teams as well as with the Technical Director, adhering closely to project plans and delivering work that meets functional & non-functional requirements. You will have the opportunity to create new, innovative, secure and scalable features for our clients on the Shopify platform
+          </p>
+          <p>
+            Want to work with us? You're in good company!
+          </p>
+        </span>
+      </div>
+      <div className={s.overview}>
+        <div className={s.salary}>
+          <h5>Зарплата</h5>
+          <p className={s.salayText}>$100,000 - $120,000</p>
+          <p>Yearly salary</p>
+        </div>
+        <div>
+          <h5>Обзор вакансии</h5>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/pages/Profile/Title/Title.tsx
+++ src/pages/Profile/Title/Title.tsx
@@ -1,14 +1,19 @@
+import { testCallback } from '../../..'
+import Button from '../../../components/Buttons/Button'
 import s from './Title.module.css'
 import image from './logo.png'
 
 const Title = () => {
     return (
         <div className={s.title}>
-            <img src={image} alt='avatar' />
-            <span>
-                <h3>Специальность</h3>
-                <h4>От компании Google</h4>
-            </span>
+            <div className={s.info}>
+                <img src={image} alt='avatar' />
+                <span>
+                    <h3>Специальность</h3>
+                    <h4>От компании Google <span>FULL-TIME</span></h4>
+                </span>
+            </div>
+            <Button callback={testCallback} text='Учавствовать' />
         </div>
     )
 }
